e2683aaaaa233625deea6e157259dd80f94d110f
jerryyu <jerryyu@uchicago.edu>
2016-06-14 16:02:17 -0700

    Couldn't find a way to style the supportActionBar using styles.xml so I did it via code instead

--- app/src/main/java/com/jerryyu/codingassignment/MainActivity.java
+++ app/src/main/java/com/jerryyu/codingassignment/MainActivity.java
@@ -18,9 +18,14 @@
 
 package com.jerryyu.codingassignment;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
@@ -80,6 +85,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 autoCompleteTextView.setText("");
             }
         });
+
+        // Make the action bar conform to material design
+        android.support.v7.app.ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.primary)));
+        supportActionBar.setTitle(Html.fromHtml("<font color='#FFFFFF'>Coding Assignment</font>"));
     }
 
     /**
